1b59cd8763709048f9e04f82423e0f5ccea07b36
AZhao <alanzzhao@gmail.com>
2018-02-13 16:31:58 -0500

    add predictions, models for individual predictions

---
+++ src/data/processed_data.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pandas as pd
+import os
+from pathlib import Path
+
+PROCESSED_DATA_DIR = os.path.join(os.path.dirname(Path(__file__).parents[1]), 'data/processed')
+
+aX_h_train = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'aX_h_train.csv'))
+ay_h_train = np.load(os.path.join(PROCESSED_DATA_DIR, 'ay_h_train.npy'))
+
+bX_h_train = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'bX_h_train.csv'))
+by_h_train = np.load(os.path.join(PROCESSED_DATA_DIR, 'by_h_train.npy'))
+
+cX_h_train = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'cX_h_train.csv'))
+cy_h_train = np.load(os.path.join(PROCESSED_DATA_DIR, 'cy_h_train.npy'))
+
+aX_i_train = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'aX_i_train.csv'))
+ay_i_train = np.load(os.path.join(PROCESSED_DATA_DIR, 'ay_i_train.npy'))
+
+bX_i_train = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'bX_i_train.csv'))
+by_i_train = np.load(os.path.join(PROCESSED_DATA_DIR, 'by_i_train.npy'))
+
+cX_i_train = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'cX_i_train.csv'))
+cy_i_train = np.load(os.path.join(PROCESSED_DATA_DIR, 'cy_i_train.npy'))
+
+a_h_test = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'a_h_test.csv'))
+b_h_test = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'b_h_test.csv'))
+c_h_test = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'c_h_test.csv'))
+
+a_i_test = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'a_i_test.csv'))
+b_i_test = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'b_i_test.csv'))
+c_i_test = pd.read_csv(os.path.join(PROCESSED_DATA_DIR, 'c_i_test.csv'))
---
+++ src/models/predict_individual_model.py
@@ -0,0 +1,33 @@
+from src.data.processed_data import a_i_test, a_h_test, b_i_test, b_h_test, c_i_test, c_h_test
+
+from src.data.processed_data import aX_h_train, ay_h_train, bX_h_train, by_h_train, cX_h_train, cy_h_train
+from src.data.processed_data import aX_i_train, ay_i_train, bX_i_train, by_i_train, cX_i_train, cy_i_train
+
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.calibration import CalibratedClassifierCV, calibration_curve
+from sklearn.externals import joblib
+from pathlib import Path
+import os
+
+MODEL_DIR = os.path.join(os.path.dirname(Path(__file__).parents[1]), 'models/')
+
+# set generic Gradient Boosting as classifier
+model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, random_state=0)
+
+model_a_cccv=CalibratedClassifierCV(model, cv=3)
+
+model_a_cccv.fit(aX_i_train, ay_i_train)
+
+model_b_cccv=CalibratedClassifierCV(model, cv=3)
+
+model_b_cccv.fit(bX_i_train, by_i_train)
+
+model_c_cccv=CalibratedClassifierCV(model, cv=3)
+
+model_c_cccv.fit(cX_i_train, cy_i_train)
+
+# persist models
+joblib.dump(model_a_cccv, os.path.join(MODEL_DIR, 'individual_a_cccv.pkl'))
+joblib.dump(model_b_cccv, os.path.join(MODEL_DIR, 'individual_b_cccv.pkl'))
+joblib.dump(model_c_cccv, os.path.join(MODEL_DIR, 'individual_c_cccv.pkl'))
+
